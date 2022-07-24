016cc07e09909f8f912ea02a064ee1ade7f98521
deniSwd <denis_swd@mail.ru>
2022-07-24 12:11:12 +0200

    24.07.2022 deniSwd was added navigate to main if cart empty

--- src/components/cartPage/CartPage.tsx
+++ src/components/cartPage/CartPage.tsx
@@ -4,12 +4,16 @@ import {selectCart} from "../../store/mainSlice"
 import {ProductType} from "../../MainTypes"
 import {ProductInCart} from "./productInCart/ProductInCart"
 import s from './CartPage.module.scss'
+import {Navigate} from "react-router-dom";
 
 export const CartPage: FC = () => {
   const cart = useAppSelector(selectCart)
 
   const totalPrice = () => {
     return cart.reduce((sum, product) => sum + Number(product.price), 0)
+  }
+  if (cart.length === 0) {
+    return <Navigate to='/'/>
   }
   return (
     <div className={s.cartPage}>
